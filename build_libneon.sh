1c7c19c9970ed3fdf9bc54ffb22badb077cb0c71
Jakub Lekstan <kuebzky@gmail.com>
2013-05-07 14:18:15 +0200

    neon: --without-gssapi

--- build_libneon.sh
+++ build_libneon.sh
@@ -5,6 +5,6 @@ BUILDDIR="$(readlink -f $BUILDDIR)/build"
 NEONDIR="$BUILDDIR/neon"
 
 if [ -n "$NEONDIR/lib/libneon.a" ]; then
-    mkdir -p $NEONDIR && cd deps/subversion/neon && ./configure --prefix=$NEONDIR && make clean install
+    mkdir -p $NEONDIR && cd deps/subversion/neon && ./configure --without-gssapi --prefix=$NEONDIR && make clean install
 fi
 
